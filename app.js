6b4a062ae503ea0cf09e62ba9b4b6fa7e45a0bf3
Bassolé Arnaud <bassolearnaud@10.235.9.148>
2024-10-15 11:45:26 +0000

    m

--- app.js
+++ app.js
@@ -15,15 +15,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 app.use(morgan("dev"));
 
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage!');
+});
 app.use("/api/users",UserRouter);
 app.use("/api/commande",midllewearDecript,commandeRouter);
 
 connecterDb()
   .then(() => {
-    app.listen(3000, () => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
       console.log("function");
     });
   })
