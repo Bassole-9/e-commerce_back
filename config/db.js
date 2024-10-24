9d3a884a0add17e3f2779e92c55df59371ce4f51
MariamWand <mariamwandaogo7@gmail.com>
2024-10-24 12:06:17 +0000

    add comment

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import { connect } from "mongoose";
 
 
-///function qui permet de connecter mon serveur a ma base de donnee
+///function qui permet de connecter mon serveur a ma base de donnee mongodb atlas
 export const connecterDb = async ()=>{
     const secret = process.env.mongoo_uri
     if(!secret)throw new Error("url n'existe pas")
